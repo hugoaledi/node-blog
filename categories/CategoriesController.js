87752ddd861953b69b359d7123971551633d17d8
hugoaledi <64563822+hugoaledi@users.noreply.github.com>
2020-05-07 22:36:56 -0300

    Cadastro de categorias

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const Category = require('./Category');
+const slugify = require('slugify');
 
 router.get('/categories', (req, res) => {
     res.send('ROTA DE CATEGORIAS');
 });
 
 router.get('/admin/categories/new', (req, res) => {
-    res.send('ROTA PARA CRIAR UMA NOVA CATEGORIA!');
+    res.render('admin/categories/new');
+});
+
+router.post('/categories/save', (req, res) => {
+    let { title } = req.body;
+    if (title) {
+        Category.create({
+            title,
+            slug: slugify(title)
+        })
+            .then(() => {
+                res.redirect('/');
+            })
+    } else {
+        res.redirect('/admin/categories/new');
+    }
 });
 
 module.exports = router;
