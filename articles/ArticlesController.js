e889107397b54f109eee6f0bd8a2bc294de6be42
hugoaledi <64563822+hugoaledi@users.noreply.github.com>
2020-05-08 17:24:10 -0300

    Salvando atigo

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const Category = require('../categories/Category');
+const Article = require('./Article');
+const slugify = require('slugify');
 
-router.get('/articles', (req, res) => {
+router.get('/admin/articles', (req, res) => {
     res.send('ROTA DE ARTIGOS');
 });
 
@@ -11,6 +13,18 @@ router.get('/admin/articles/new', (req, res) => {
         .then(categories => {
             res.render('admin/articles/new', { categories });
         });
+});
+
+router.post('/admin/article/save', (req, res) => {
+    const { title, body, category } = req.body;
+    Article.create({
+        title,
+        slug: slugify(title),
+        body,
+        categoryId: category
+    }).then(() => {
+        res.redirect('/admin/articles');
+    });
 });
 
 module.exports = router;
