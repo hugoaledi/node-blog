fe0624bb7d97d3d90e034df68465a35cc55fca65
hugoaledi <64563822+hugoaledi@users.noreply.github.com>
2020-05-09 10:05:51 -0300

    Implementando login de usuário por session

--- users/UsersController.js
+++ users/UsersController.js
@@ -35,4 +35,29 @@ router.post('/user/create', (req, res) => {
         });
 });
 
+router.get('/login', (req, res) => {
+    res.render('admin/users/login');
+});
+
+router.post('/authenticate', (req, res) => {
+    const { email, password } = req.body;
+    User.findOne({ where: { email } })
+        .then(user => {
+            if (user != undefined) {
+                const correct = bcrypt.compareSync(password, user.password);
+                if (correct) {
+                    req.session.user = {
+                        id: user.id,
+                        email: user.email
+                    };
+                    res.json(req.session.user);
+                } else {
+                    res.redirect('/login');
+                }
+            } else {
+                res.redirect('/login');
+            }
+        });
+});
+
 module.exports = router;
