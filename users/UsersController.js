e6cd0856e2ea1c997d01890d10938cf5b71c8cb2
hugoaledi <64563822+hugoaledi@users.noreply.github.com>
2020-05-09 10:19:51 -0300

    Implementando logout

--- users/UsersController.js
+++ users/UsersController.js
@@ -60,4 +60,9 @@ router.post('/authenticate', (req, res) => {
         });
 });
 
+router.get('/logout', (req, res) => {
+    req.session.user = undefined;
+    res.redirect('/');
+});
+
 module.exports = router;
